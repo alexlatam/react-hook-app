479e9cfd35f4a4affb1e7ba338961e7d9ac0f831
Alexis Montilla <alexisamm9261@gmail.com>
2023-04-25 10:45:55 -0400

    feature task resolved

---
+++ src/06-memos/CallbackHook.jsx
@@ -0,0 +1,24 @@
+import { useCallback, useState } from "react";
+import { ShowIncrement } from "./ShowIncrement";
+
+export const CallbackHook = () => {
+  const [counter, setCounter] = useState(10);
+
+  // Aqui tenemos una funcion memorizada
+  const increment = useCallback((value) => {
+    setCounter((total) => total + value);
+  }, []);
+
+  // const increment = () => {
+  //     setCounter(counter + 1)
+  // }
+
+  return (
+    <>
+      <h1>useCallback Hook: {counter}</h1>
+      <hr />
+
+      <ShowIncrement increment={increment} />
+    </>
+  );
+};
--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 // import { FocusScreen } from './04-useRef/FocusScreen.jsx';
 // import { Layout } from './05-useLayoutEffect/Layout.jsx';
 // import { Memorize } from './06-memos/Memorize.jsx';
-import { MemorizeHook } from './06-memos/MemorizeHook.jsx';
+// import { MemorizeHook } from './06-memos/MemorizeHook.jsx';
+// import { CallbackHook } from './06-memos/CallbackHook.jsx';
+import { Padre } from './07-tarea-memo/Padre.jsx';
 
 import './index.css'
 
@@ -26,6 +28,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     {/* <FocusScreen /> */}
     {/* <Layout /> */}
     {/* <Memorize /> */}
-    <MemorizeHook />
+    {/* <MemorizeHook /> */}
+    {/* <CallbackHook /> */}
+    <Padre />
   </React.StrictMode>,
 )
